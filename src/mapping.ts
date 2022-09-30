cfea8fb2fb66fb8c220f5eddb71a01a6519e60f1
mythcsj <mythcsj@163.com>
2022-09-30 16:14:32 +0800

    graph init

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,56 +5,46 @@ import {
   ApprovalForAll,
   Transfer
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { EventLog, ExampleEntity } from "../generated/schema"
 
 export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
+
   let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
+
   if (!entity) {
     entity = new ExampleEntity(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity.count = BigInt.zero()
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  entity.count = entity.count.plus(BigInt.fromI32(1)) 
 
-  // Entity fields can be set based on event parameters
   entity.owner = event.params.owner
   entity.spender = event.params.spender
 
-  // Entities can be written to the store with `.save()`
   entity.save()
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.tokenIsAtIndex(...)
-  // - contract.userOwnedTokens(...)
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
+export function handleApprovalForAll(event: ApprovalForAll): void {
+  let log =  EventLog.load('ApprovalForAll');
+  if(!log){
+    log = new EventLog('ApprovalForAll');
+    log.count = BigInt.fromI32(1);
+  } 
+  if(log) log.count = log.count.plus(BigInt.fromI32(1)) ;
+  log.action = 'ApprovalForAll'
+  log.save();
+}
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: Transfer): void {
+  let log =  EventLog.load('Transfer');
+  if(!log){
+    log = new EventLog('Transfer');
+    log.count = BigInt.fromI32(1);
+  } 
+  if(log) log.count = log.count.plus(BigInt.fromI32(1)) ;
+  log.action = 'Transfer'
+  log.save();
+}
